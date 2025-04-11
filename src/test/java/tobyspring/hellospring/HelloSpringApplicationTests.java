e02b95e8b96102c621c167dbe1dce43acbe5bdfc
su <su@herit.net>
2025-04-11 17:10:58 +0900

    creat helloSpring

---
+++ src/test/java/tobyspring/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
