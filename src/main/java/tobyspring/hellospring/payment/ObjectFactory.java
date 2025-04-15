f248d5a71bdd07a9fb727efcabc0a71ff76ef66d
su <su@herit.net>
2025-04-15 10:53:24 +0900

    스프링 컨테이너 == BeanFactory * Configutation을 생성해 beanFactory에 의존관계의 정보를 제공한다

--- src/main/java/tobyspring/hellospring/payment/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/payment/ObjectFactory.java
@@ -1,9 +1,15 @@
 package tobyspring.hellospring.payment;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
+    @Bean
     public ExRateProvider exRateProvider() {
         return new WebApiExRateProvider();
     }
